1ca67626ae9d9cebc03c2f32d9e574d9077bd0d2
nikhilbaloji <nikhilbaloji@gmail.com>
2025-11-10 20:42:05 +0530

    save

---
+++ studentsys.py
@@ -0,0 +1,15 @@
+import sys
+if len(sys.argv) == 3:
+    script_name = sys.argv[0]
+    name = sys.argv[1]
+    rollno = sys.argv[2]
+    print("User provided input values:")
+else:
+    script_name = sys.argv[0]
+    name = "Nikhil"
+    rollno = "032"
+    print("No input given - using default values:")
+
+    print("Script name:", script_name)
+    print("Student Name:", name)
+    print("Roll Number:", rollno)
