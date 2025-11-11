6203d5b8256f81547e1d29136461046e87dd6f9a
nikhilbaloji <nikhilbaloji@gmail.com>
2025-11-11 08:53:57 +0530

    Update studentsys.py

--- studentsys.py
+++ studentsys.py
@@ -1,15 +1,19 @@
-import sys
-if len(sys.argv) == 3:
-    script_name = sys.argv[0]
-    name = sys.argv[1]
-    rollno = sys.argv[2]
-    print("User provided input values:")
-else:
-    script_name = sys.argv[0]
-    name = "Nikhil"
-    rollno = "032"
-    print("No input given - using default values:")
+import sys;
+if len(sys.argv) == 5:
+   script_name = sys.argv[0]
+   name = sys.argv[1]
+   marks1 = sys.argv[2]
+   marks2 = sys.argv[3]
+   marks3= sys.argv[4]
+else  :
+   script_name = sys.argv[0]
+   name = "nikhil"
+   marks1 = 20
+   marks2 = 30
+   marks3= 40
 
-    print("Script name:", script_name)
-    print("Student Name:", name)
-    print("Roll Number:", rollno)
+sum = marks1+marks2+marks3
+avg = sum/3
+print(f"Sum {sum}\n")
+print(f"Average: {avg}")
+print(f"Thansk For Using program")
